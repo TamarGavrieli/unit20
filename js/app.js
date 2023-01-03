24dc0d5ed582b2d80d7490f0650ce43a6d062837
Cheli Adani Hacohen <adanihac@g.jct.ac.il>
2023-01-03 09:31:02 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,80 @@
 /*import { drawBoard } from './draw_board.js'*/
+
+const levels = [
+    [
+        [0,8,0, 0,9,6, 0,3,0,
+        0,2,0, 7,4,0, 0,0,8,
+            9,0,4, 0,0,0, 2,0,0,
+            0,0,6, 8,0,0, 0,1,2,
+            1,3,0, 0,6,0, 0,5,4,
+            8,9,0, 0,0,1, 7,0,0,
+            0,0,9, 0,0,0, 1,0,6,
+            6,0,0, 0,7,2, 0,4,0,
+            0,1,0, 6,8,0, 0,2,0],
+
+           [0,3,6, 2,0,0, 0,4,0,
+            0,0,8, 4,9,0, 6,0,1,
+            0,0,1, 0,5,0, 7,0,0,
+            7,0,0, 0,0,8, 0,6,0,
+            0,5,9, 0,0,7, 1,3,0,
+            0,6,0, 0,4,5, 0,0,8,
+            0,0,4, 0,6,0, 9,0,0,
+            2,0,5, 0,1,4, 3,0,0,
+            0,1,0, 0,0,9, 4,5,0] 
+        ],
+        [  [7,0,0, 8,0,0, 0,1,0,
+            1,0,0, 0,0,3, 8,0,0,
+            0,5,0, 0,6,0, 0,0,2,
+            2,0,0, 6,0,0, 4,0,9,
+            3,0,0, 9,0,4, 0,0,5,
+            9,0,4, 0,0,5, 0,0,1,
+            5,0,0, 0,4,0, 0,2,0,
+            0,0,1, 7,0,0, 0,0,8,
+            0,8,0, 0,0,2, 0,0,3],
+
+         [0,0,2, 9,0,0, 0,6,7,
+            0,0,6, 3,0,0, 2,0,0,
+            0,0,3, 0,0,6, 0,1,0,
+            0,0,8, 0,0,3, 5,0,0,
+            0,1,0, 0,7,0, 0,2,0,
+            0,0,4, 0,1,8, 6,0,0,
+            0,2,0, 4,0,0, 7,0,0,
+            0,0,9, 0,0,2, 1,0,0,
+            3,5,0, 0,0,9, 4,0,0] ],
+
+[            [0,0,5, 0,1,0, 0,0,0,
+            0,0,9, 0,0,8, 6,0,0,
+            6,0,0, 7,2,0, 0,3,0,
+            8,0,0, 0,0,0, 0,0,0,
+            0,0,2, 1,0,0, 5,0,0,
+            0,0,0, 0,4,0, 0,0,7,
+            0,7,0, 0,6,1, 0,0,8,
+            0,0,4, 9,0,0, 2,0,0,
+            0,6,0, 0,5,0, 3,0,0],
+
+          [4,0,0, 5,0,0, 0,0,6,
+            0,3,0, 0,0,1, 0,5,0,
+            0,0,0, 0,0,6, 0,0,2,
+            7,0,0, 0,3,0, 0,2,0,
+            1,0,0, 0,9,2, 0,0,3,
+            0,9,0, 0,6,0, 0,0,5,
+            6,0,0, 3,0,0, 0,0,0,
+            0,5,0, 4,0,0, 0,8,0,
+            9,0,0, 0,0,8, 0,0,7] 
+        ]
+  ];
+
 function drawBoard() {
-    const sudokuBoard = document.querySelector("#puzzle")
-    const squares = 81
+    const sudokuBoard = document.querySelector("#puzzle");
+    const squares = 81;
+
+  
 
     for (let i=0; i<squares; i++) {
-        const inputElement = document.createElement("input")
-        inputElement.setAttribute('type', 'number')
-        inputElement.setAttribute('min', '1')
-        inputElement.setAttribute('max', '9')
+        const inputElement = document.createElement("input");
+        inputElement.setAttribute('type', 'number');
+        inputElement.setAttribute('min', '1');
+        inputElement.setAttribute('max', '9');
 
         if (
             ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
@@ -16,22 +83,34 @@ function drawBoard() {
             ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
             ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
         ) {
-            inputElement.classList.add('odd-section')
+            inputElement.classList.add('odd-section');
         }
 
-        sudokuBoard.appendChild(inputElement)
+        sudokuBoard.appendChild(inputElement);
     }
 }
 
-function main() {
-    drawBoard()
-
+function loadRandomBoard() {
+    const inputs = document.querySelectorAll('input');
+    let index = Math.floor(Math.random()*size);
+    let savedBoard = levels[0][index];
     
+    for(let i=0; i<savedBoard.length; i++) {
+        if(savedBoard[i] == 0) {
+             inputs[i].value = ""
+             inputs[i].classList.add('empty')
+        } else {
+            inputs[i].value = savedBoard[i]
+            inputs[i].classList.add('knowledge')
+            inputs[i].readOnly = true
+        }
+    }
+    savedBoard = []
+}
 
-  
-
-
-  
+function main() {
+    drawBoard();
+    loadRandomBoard();
 }
 
 main()
