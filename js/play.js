7aa11400814443d41920fe24743f6d8171f2ad31
Cheli Adani Hacohen <adanihac@g.jct.ac.il>
2023-01-10 15:28:06 +0200

    qwe

--- js/play.js
+++ js/play.js
@@ -1,53 +1,59 @@
+var game_name = "animtion";
+var game_level = "easy";
+function before() {
+  document.getElementById('myImage')
+    .src = "../image/italy3.jpg";
+  document.getElementById('message')
+    .innerHTML = "Hii! GeeksforGeeks people";
+}
 
-	function before(){
-		document.getElementById('myImage')
-		.src="../image/italy3.jpg";
-		document.getElementById('message')
-		.innerHTML="Hii! GeeksforGeeks people";
-	}
-	
-	function afterr(){
-		document.getElementById('myImage')
-		.src="../image/italy2.jpg";
-		document.getElementById('message')
-		.innerHTML="Bye! GeeksforGeeks people";
-	}
-	
+function afterr() {
+  document.getElementById('myImage')
+    .src = "../image/italy2.jpg";
+  document.getElementById('message')
+    .innerHTML = "Bye! GeeksforGeeks people";
+}
 
-	
+
+
 function changeImageT(x) {
-    findLetter(x);
-  }
- 
-  function findLetter(l){
-    let letABC= document.getElementById(l.toString());
-    letABC.src ="../image/"+ l+".png";
-	score();
-  }
+  findLetter(x);
+}
 
-  function levelHard(l){
-    document.location="../html/hard.html";
-  }
-  function levelAdvance(l){
-	document.location="../html/advance.html";
-  }
+function findLetter(l) {
+  let letABC = document.getElementById(l.toString());
+  letABC.src = "../image/" + l + ".png";
+  score();
+}
 
-  function levelEasy(l){
-	let x= document.getElementById(l.toString());
-	
-	document.location="../html/easy.html";
-  }
+function levelHard(l) {
+  game_level = "hard";
+  document.location = "../html/hard.html";
+}
+function levelAdvance(l) {
+  game_level = "advance";
+  document.location = "../html/advance.html";
+}
+
+function levelEasy(l) {
+  game_level = "easy";
+  let x = document.getElementById(l.toString());
+  document.location = "../html/easy.html";
+}
 
 
-  function score(){
+function score() {
   const phone = document.cookie;
-  alert(phone+ "jjj");
   const emailvalue = phone.substring(phone.indexOf('=') + 1);
+  // alert("emailvalue: "+emailvalue);
   let user = localStorage.getItem(emailvalue);
+  // alert("user: "+user);
   let userjson = JSON.parse(user);
+  let corrtime = new Date();
   userjson.score += 5;
+ const userPlay = { gname: game_name, glevel: game_level, cscore: userjson.cscore, date: corrtime.getDate(), time: corrtime.getTime() };
+  userjson.arr.push(JSON.stringify(userPlay));
   let myscore= userjson.score;
   window.localStorage.setItem(emailvalue, JSON.stringify(userjson));
-  alert(myscore);
-
-  }
+  // alert(myscore);
+}
